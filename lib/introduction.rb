569830893c3baa89099e5716c4c65fda4fa0c52e
John Chen <johnchen062094@gmail.com>
2020-04-25 21:33:14 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,3 +2,5 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
+def introduction_with_language(name, program_type)
+  puts "Hi, my name is #{name} and I am learning to program in #{program_type}."
